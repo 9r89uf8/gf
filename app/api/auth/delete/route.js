a63b2f6eaeea2860af6cd5435327ee7885a5b6e7
unknown <silvanovaldez53@outlook.com>
2024-09-29 19:09:24 -0500

    jlj

--- app/api/auth/delete/route.js
+++ app/api/auth/delete/route.js
@@ -6,6 +6,7 @@ import {NextResponse} from "next/server";
 export const dynamic = 'force-dynamic';
 
 
+
 export async function GET(req) {
     try {
         const authResult = await authMiddleware(req);
