e83c6f52c4b7bb0177d5d1a9c82f8a54fbb8debb
unknown <silvanovaldez53@outlook.com>
2024-10-02 10:28:47 -0500

    jlj

--- app/layout.js
+++ app/layout.js
@@ -52,9 +52,6 @@ const Layout = ({ children }) => {
 
             <link rel="icon" href="/favicon.ico"/>
             <link rel="manifest" href="/manifest.json"/>
-            <meta name="theme-color" content="#FF4081"/>
-
-
             <meta name="robots" content="index, follow"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
