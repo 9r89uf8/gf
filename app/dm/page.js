b2d290d7dcd97006f8273c77236797ecbbca0c87
unknown <silvanovaldez53@outlook.com>
2024-10-11 19:51:15 -0500

    jlj

--- app/dm/page.js
+++ app/dm/page.js
@@ -14,7 +14,9 @@ import {
     ListItemAvatar,
     Avatar,
     ListItemText,
-    IconButton, Button,
+    Button,
+    Snackbar,
+    Alert,
 } from '@mui/material';
 import MessageIcon from '@mui/icons-material/Message';
 
@@ -22,6 +24,7 @@ const DMList = () => {
     const girls = useStore((state) => state.girls);
     const [searchTerm, setSearchTerm] = useState('');
     const [loading, setLoading] = useState(true);
+    const [openSnackbar, setOpenSnackbar] = useState(false); // State for Snackbar
 
     useEffect(() => {
         async function fetchGirls() {
@@ -46,92 +49,71 @@ const DMList = () => {
     return (
         <>
             {/* Search Bar */}
-            <Box sx={{ display: 'flex', justifyContent: 'center', mb: 4 }}>
-                <TextField
-                    placeholder="Buscar creadores..."
-                    variant="outlined"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    sx={{
-                        width: '100%',
-                        maxWidth: '300px', // Made the search bar smaller
-                        '& .MuiOutlinedInput-root': {
-                            borderRadius: '25px',
-                            backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                            '&:hover': {
-                                backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                <Box sx={{ display: 'flex', justifyContent: 'center', mb: 4 }}>
+                    <TextField
+                        placeholder="Buscar creadores..."
+                        variant="outlined"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        sx={{
+                            width: '100%',
+                            maxWidth: '300px',
+                            '& .MuiOutlinedInput-root': {
+                                borderRadius: '25px',
+                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                                '&:hover': {
+                                    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                                },
+                                '& fieldset': {
+                                    borderColor: 'rgba(255, 255, 255, 0.3)',
+                                },
                             },
-                            '& fieldset': {
-                                borderColor: 'rgba(255, 255, 255, 0.3)',
+                            '& .MuiOutlinedInput-input': {
+                                color: 'white',
                             },
-                        },
-                        '& .MuiOutlinedInput-input': {
-                            color: 'white',
-                        },
-                        '& .MuiInputLabel-root': {
-                            color: 'rgba(255, 255, 255, 0.7)',
-                        },
-                    }}
-                />
-            </Box>
+                            '& .MuiInputLabel-root': {
+                                color: 'rgba(255, 255, 255, 0.7)',
+                            },
+                        }}
+                    />
+                </Box>
 
             {/* Users List */}
-            <List>
-                {filteredUsers.map((user, index) => (
-                    <ListItem
-                        key={index}
-                        sx={{
-                            backgroundColor: 'transparent',
-                            color: 'white',
-                            '&:hover': {
-                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                            },
-                            mb: 2, // Added margin between users
-                        }}
-                    >
-                        <ListItemAvatar>
-                            <Link href={`/${user.id}`} passHref legacyBehavior>
-                                <Avatar
-                                    src={`https://d3sog3sqr61u3b.cloudfront.net/${user.picture}`}
-                                    alt={user.username}
-                                    sx={{
-                                        width: 100,
-                                        height: 100,
-                                        cursor: 'pointer', // Indicates the avatar is clickable
-                                    }}
-                                />
-                            </Link>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={user.username}
-                            sx={{ color: 'white', padding: 1 }}
-                        />
-                        {user.private?
-                            <Button
-                                sx={{
-                                    background: 'linear-gradient(45deg, #212529 30%, #343a40 90%)',
-                                    border: 0,
-                                    fontSize: 18,
-                                    borderRadius: 25,
-                                    boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .1)',
-                                    color: 'white',
-                                    height: 48,
-                                    padding: '0 15px',
-                                    fontWeight: 'bold',
-                                    textTransform: 'none',
-                                    '&:hover': {
-                                        background: 'linear-gradient(45deg, #adb5bd 30%, #343a40 90%)',
-                                    },
-                                }}
-                            >
-                                Privado
-                            </Button>
-
-                            :
-                            <Link href='/chat' passHref legacyBehavior>
+                <List>
+                    {filteredUsers.map((user, index) => (
+                        <ListItem
+                            key={index}
+                            sx={{
+                                backgroundColor: 'transparent',
+                                color: 'white',
+                                '&:hover': {
+                                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                                },
+                                mb: 2,
+                            }}
+                        >
+                            <ListItemAvatar>
+                                <Link href={`/${user.id}`} passHref legacyBehavior>
+                                    <Avatar
+                                        src={`https://d3sog3sqr61u3b.cloudfront.net/${user.picture}`}
+                                        alt={user.username}
+                                        sx={{
+                                            width: 100,
+                                            height: 100,
+                                            cursor: 'pointer',
+                                        }}
+                                    />
+                                </Link>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={user.username}
+                                sx={{ color: 'white', padding: 1 }}
+                                primaryTypographyProps={{ fontSize: '1.5rem' }} // Increased font size
+                            />
+                            {user.private ? (
                                 <Button
                                     sx={{
-                                        background: 'linear-gradient(45deg, #0096c7 30%, #023e8a 90%)',
+                                        background: 'linear-gradient(45deg, #212529 30%, #343a40 90%)',
                                         border: 0,
                                         fontSize: 18,
                                         borderRadius: 25,
@@ -142,22 +124,60 @@ const DMList = () => {
                                         fontWeight: 'bold',
                                         textTransform: 'none',
                                         '&:hover': {
-                                            background: 'linear-gradient(45deg, #FE8B8B 30%, #FFAE53 90%)',
+                                            background: 'linear-gradient(45deg, #adb5bd 30%, #343a40 90%)',
                                         },
                                     }}
+                                    onClick={() => setOpenSnackbar(true)} // Open Snackbar on click
                                 >
-                                    Mensaje
+                                    Privado
                                 </Button>
-                            </Link>
-                        }
+                            ) : (
+                                <Link href='/chat' passHref legacyBehavior>
+                                    <Button
+                                        sx={{
+                                            background: 'linear-gradient(45deg, #0096c7 30%, #023e8a 90%)',
+                                            border: 0,
+                                            fontSize: 18,
+                                            borderRadius: 25,
+                                            boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .1)',
+                                            color: 'white',
+                                            height: 48,
+                                            padding: '0 15px',
+                                            fontWeight: 'bold',
+                                            textTransform: 'none',
+                                            '&:hover': {
+                                                background: 'linear-gradient(45deg, #FE8B8B 30%, #FFAE53 90%)',
+                                            },
+                                        }}
+                                    >
+                                        Mensaje
+                                    </Button>
+                                </Link>
+                            )}
+                        </ListItem>
+                    ))}
+                </List>
 
-                    </ListItem>
-                ))}
-            </List>
+                {/* Snackbar to display the message */}
+                <Snackbar
+                    open={openSnackbar}
+                    autoHideDuration={6000}
+                    onClose={() => setOpenSnackbar(false)}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                >
+                    <Alert
+                        onClose={() => setOpenSnackbar(false)}
+                        severity="info"
+                        sx={{ width: '100%' }}
+                    >
+                        La chica ha configurado su cuenta como privada y no puede recibir mensajes.
+                    </Alert>
+                </Snackbar>
         </>
     );
 };
 
 export default DMList;
+
 
 
