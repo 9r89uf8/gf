9ae20b189ab306159619070d75dc0c4c9f70fb60
unknown <silvanovaldez53@outlook.com>
2024-10-15 21:03:04 -0500

    jlj

--- app/components/landing/PopularCreators.js
+++ app/components/landing/PopularCreators.js
@@ -12,7 +12,7 @@ import {
     Button,
     Card,
     Grid,
-    CircularProgress,
+    Skeleton,
 } from '@mui/material';
 import VerifiedIcon from "@mui/icons-material/Verified";
 import CakeIcon from "@mui/icons-material/Cake";
@@ -21,6 +21,7 @@ const PopularCreators = () => {
     const girls = useStore((state) => state.girls);
     const [searchTerm, setSearchTerm] = useState('');
     const [loading, setLoading] = useState(true);
+    const [visibleUsersCount, setVisibleUsersCount] = useState(4);
 
     useEffect(() => {
         async function fetchGirls() {
@@ -34,16 +35,7 @@ const PopularCreators = () => {
         user.username.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    // Limit to first 5 creators
-    const limitedUsers = filteredUsers.slice(0, 5);
-
-    if (loading) {
-        return (
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                <CircularProgress />
-            </Box>
-        );
-    }
+    const limitedUsers = filteredUsers.slice(0, visibleUsersCount);
 
     return (
         <>
@@ -82,7 +74,7 @@ const PopularCreators = () => {
                 Creadoras Populares
             </Typography>
             <Grid container spacing={4}>
-                {limitedUsers.map((user, index) => (
+                {(loading ? Array.from(new Array(visibleUsersCount)) : limitedUsers).map((user, index) => (
                     <Grid item xs={12} sm={6} md={4} key={index}>
                         <Card
                             sx={{
@@ -96,117 +88,127 @@ const PopularCreators = () => {
                                 padding: 3,
                             }}
                         >
-                            {/* User Card */}
-                            <Box
-                                sx={{
-                                    borderRadius: '50%',
-                                    width: { xs: 150, md: 200 },
-                                    height: { xs: 150, md: 200 },
-                                    overflow: 'hidden',
-                                    border: '4px solid rgba(255, 255, 255, 0.5)',
-                                    boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)',
-                                    margin: '0 auto',
-                                }}
-                            >
-                                <Link href={`/${user.id}`} passHref legacyBehavior>
-                                    <img
-                                        src={`https://d3sog3sqr61u3b.cloudfront.net/${user.picture}`}
-                                        alt={user.name}
-                                        style={{
-                                            width: '100%',
-                                            height: '100%',
-                                            objectFit: 'cover',
-                                        }}
-                                    />
-                                </Link>
-                            </Box>
-                            <Box display="flex" justifyContent="center">
-                                <Typography variant="h5" gutterBottom fontWeight="bold" sx={{ color: 'white', marginTop: 1 }}>
-                                    {user.username}
-                                    <VerifiedIcon
-                                        sx={{ ml: 1, verticalAlign: 'middle', color: '#4FC3F7', fontSize: 36 }}
-                                    />
-                                </Typography>
-                            </Box>
-                            <Box display="flex" alignItems="center" justifyContent="center" mb={2}>
-                                <CakeIcon sx={{ mr: 1, fontSize: 36 }} />
-                                <Typography variant="h5">{user.age}</Typography>
-                            </Box>
-                            <Box display="flex" justifyContent="center" mt={2}>
-                                <Link href={`/${user.id}`} passHref legacyBehavior>
-                                    <Button
+                            {loading ? (
+                                <>
+                                    <Skeleton variant="circular" width={150} height={150} sx={{ margin: '0 auto' }} />
+                                    <Skeleton variant="text" sx={{ fontSize: '1.5rem', mt: 2 }} />
+                                    <Skeleton variant="text" sx={{ fontSize: '1rem', mt: 1 }} />
+                                    <Skeleton variant="rectangular" height={48} sx={{ mt: 2, borderRadius: '25px' }} />
+                                </>
+                            ) : (
+                                <>
+                                    {/* User Card */}
+                                    <Box
                                         sx={{
-                                            background: 'linear-gradient(45deg, #023e8a 30%, #03045e 90%)',
-                                            border: 0,
-                                            fontSize: 18,
-                                            borderRadius: 25,
-                                            marginRight: 4,
-                                            boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .1)',
-                                            color: 'white',
-                                            height: 48,
-                                            padding: '0 15px',
-                                            fontWeight: 'bold',
-                                            textTransform: 'none',
-                                            '&:hover': {
-                                                background: 'linear-gradient(45deg, #FE8B8B 30%, #FFAE53 90%)',
-                                            },
+                                            borderRadius: '50%',
+                                            width: { xs: 150, md: 200 },
+                                            height: { xs: 150, md: 200 },
+                                            overflow: 'hidden',
+                                            border: '4px solid rgba(255, 255, 255, 0.5)',
+                                            boxShadow: '0 4px 12px rgba(0, 0, 0, 0.2)',
+                                            margin: '0 auto',
                                         }}
                                     >
-                                        Fotos
-                                    </Button>
-                                </Link>
-                                <Link href={`/chat/${user.id}`} passHref legacyBehavior>
-                                    <Button
-                                        sx={{
-                                            background: 'linear-gradient(45deg, #0096c7 30%, #023e8a 90%)',
-                                            border: 0,
-                                            fontSize: 18,
-                                            borderRadius: 25,
-                                            boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .1)',
-                                            color: 'white',
-                                            height: 48,
-                                            padding: '0 15px',
-                                            fontWeight: 'bold',
-                                            textTransform: 'none',
-                                            '&:hover': {
-                                                background: 'linear-gradient(45deg, #FE8B8B 30%, #FFAE53 90%)',
-                                            },
-                                        }}
-                                    >
-                                        Mensaje
-                                    </Button>
-                                </Link>
-                            </Box>
+                                        <Link href={`/${user.id}`} passHref legacyBehavior>
+                                            <img
+                                                src={`https://d3sog3sqr61u3b.cloudfront.net/${user.picture}`}
+                                                alt={user.name}
+                                                style={{
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    objectFit: 'cover',
+                                                }}
+                                            />
+                                        </Link>
+                                    </Box>
+                                    <Box display="flex" justifyContent="center">
+                                        <Typography variant="h5" gutterBottom fontWeight="bold" sx={{ color: 'white', marginTop: 1 }}>
+                                            {user.username}
+                                            <VerifiedIcon
+                                                sx={{ ml: 1, verticalAlign: 'middle', color: '#4FC3F7', fontSize: 36 }}
+                                            />
+                                        </Typography>
+                                    </Box>
+                                    <Box display="flex" alignItems="center" justifyContent="center" mb={2}>
+                                        <CakeIcon sx={{ mr: 1, fontSize: 36 }} />
+                                        <Typography variant="h5">{user.age}</Typography>
+                                    </Box>
+                                    <Box display="flex" justifyContent="center" mt={2}>
+                                        <Link href={`/${user.id}`} passHref legacyBehavior>
+                                            <Button
+                                                sx={{
+                                                    background: 'linear-gradient(45deg, #023e8a 30%, #03045e 90%)',
+                                                    border: 0,
+                                                    fontSize: 18,
+                                                    borderRadius: 25,
+                                                    marginRight: 4,
+                                                    boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .1)',
+                                                    color: 'white',
+                                                    height: 48,
+                                                    padding: '0 15px',
+                                                    fontWeight: 'bold',
+                                                    textTransform: 'none',
+                                                    '&:hover': {
+                                                        background: 'linear-gradient(45deg, #FE8B8B 30%, #FFAE53 90%)',
+                                                    },
+                                                }}
+                                            >
+                                                Fotos
+                                            </Button>
+                                        </Link>
+                                        <Link href={`/chat/${user.id}`} passHref legacyBehavior>
+                                            <Button
+                                                sx={{
+                                                    background: 'linear-gradient(45deg, #0096c7 30%, #023e8a 90%)',
+                                                    border: 0,
+                                                    fontSize: 18,
+                                                    borderRadius: 25,
+                                                    boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .1)',
+                                                    color: 'white',
+                                                    height: 48,
+                                                    padding: '0 15px',
+                                                    fontWeight: 'bold',
+                                                    textTransform: 'none',
+                                                    '&:hover': {
+                                                        background: 'linear-gradient(45deg, #FE8B8B 30%, #FFAE53 90%)',
+                                                    },
+                                                }}
+                                            >
+                                                Mensaje
+                                            </Button>
+                                        </Link>
+                                    </Box>
+                                </>
+                            )}
                         </Card>
                     </Grid>
                 ))}
             </Grid>
 
-            {/* "View All Creators" Button */}
-            {filteredUsers.length > 5 && (
+            {/* "More" Button */}
+            {filteredUsers.length > visibleUsersCount && (
                 <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-                    <Link href="/creators" passHref legacyBehavior>
-                        <Button
-                            variant="contained"
-                            sx={{
-                                background: 'linear-gradient(45deg, #f8f9fa 30%, #e9ecef 90%)',
-                                marginTop: -2,
-                                borderRadius: 25,
-                                marginBottom: 10,
-                                padding: '10px 30px',
-                                fontSize: 16,
-                                fontWeight: 'bold',
-                                color: 'black',
-                                textTransform: 'none',
-                                boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .2)',
-                                '&:hover': {
-                                    background: 'linear-gradient(45deg, #FFAE53 30%, #FE8B8B 90%)',
-                                },
-                            }}
-                        >
-                            Ver Todas las Creadoras
-                        </Button>
-                    </Link>
+                    <Button
+                        variant="contained"
+                        onClick={() => setVisibleUsersCount(visibleUsersCount + 4)}
+                        sx={{
+                            background: 'linear-gradient(45deg, #f8f9fa 30%, #e9ecef 90%)',
+                            marginTop: -2,
+                            borderRadius: 25,
+                            marginBottom: 10,
+                            padding: '10px 30px',
+                            fontSize: 16,
+                            fontWeight: 'bold',
+                            color: 'black',
+                            textTransform: 'none',
+                            boxShadow: '0 3px 5px 2px rgba(255, 255, 255, .2)',
+                            '&:hover': {
+                                background: 'linear-gradient(45deg, #FFAE53 30%, #FE8B8B 90%)',
+                            },
+                        }}
+                    >
+                        More
+                    </Button>
                 </Box>
             )}
         </>
@@ -214,5 +216,6 @@ const PopularCreators = () => {
 };
 
 export default PopularCreators;
+
 
 
